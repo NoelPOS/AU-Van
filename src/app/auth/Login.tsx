15505f367d4eb69e640d49808017ccc5dca3f288
Jan Poonthong <janpoonthong628@gmail.com>
2024-07-12 09:29:25 +0700

    format

--- src/app/auth/Login.tsx
+++ src/app/auth/Login.tsx
@@ -1,54 +1,54 @@
-import React, { Dispatch, SetStateAction } from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
+import React, { Dispatch, SetStateAction } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
-import Car from '../assets/img/Car.png'
-import Logo from '../assets/img/Logo.png'
+import Car from "../assets/img/Car.png";
+import Logo from "../assets/img/Logo.png";
 
 interface Props {
-  showLogin: boolean
-  setShowLogin: Dispatch<SetStateAction<boolean>>
+  showLogin: boolean;
+  setShowLogin: Dispatch<SetStateAction<boolean>>;
 }
 
 const Login = (props: Props) => {
   return (
-    <div className='w-full lg:grid lg:min-h-[500px] lg:grid-cols-2 xl:min-h-[580px] flex items-center justify-center '>
-      <div className='hidden bg-muted lg:block'>
+    <div className="w-full lg:grid lg:min-h-[500px] lg:grid-cols-2 xl:min-h-[580px] flex items-center justify-center ">
+      <div className="hidden bg-muted lg:block">
         <Image
           src={Car}
-          alt='Image'
-          className=' object-cover dark:brightness-[0.2] dark:grayscale rounded-lg'
+          alt="Image"
+          className=" object-cover dark:brightness-[0.2] dark:grayscale rounded-lg"
         />
       </div>
-      <div className='flex items-center justify-center py-12'>
-        <div className='mx-auto grid w-[350px] gap-6'>
-          <div className='grid gap-2 text-center'>
-            <div className='flex flex-col items-center justify-center'>
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <div className="flex flex-col items-center justify-center">
               <Image
                 src={Logo}
-                alt='Image'
-                className=' object-cover dark:brightness-[0.2] dark:grayscale rounded-lg'
+                alt="Image"
+                className=" object-cover dark:brightness-[0.2] dark:grayscale rounded-lg"
               />
-              <h2 className='text-3xl font-bold'>AU Van Service </h2>
+              <h2 className="text-3xl font-bold">AU Van Service </h2>
             </div>
           </div>
-          <div className='grid gap-4'>
-            <div className='grid gap-2'>
-              <Label htmlFor='email'>Email</Label>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
               <Input
-                id='email'
-                type='email'
-                placeholder='m@example.com'
+                id="email"
+                type="email"
+                placeholder="m@example.com"
                 required
               />
             </div>
-            <div className='grid gap-2'>
-              <div className='flex items-center'>
-                <Label htmlFor='password'>Password</Label>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
                 {/* <Link
                   href='/forgot-password'
                   className='ml-auto inline-block text-sm underline'
@@ -56,22 +56,22 @@ const Login = (props: Props) => {
                   Forgot your password?
                 </Link> */}
               </div>
-              <Input id='password' type='password' required />
+              <Input id="password" type="password" required />
             </div>
-            <Button type='submit' className='w-full'>
+            <Button type="submit" className="w-full">
               Login
             </Button>
             {/* <Button variant='outline' className='w-full'>
               Login with Google
             </Button> */}
           </div>
-          <div className='mt-4 text-center text-sm'>
-            Don&apos;t have an account?{' '}
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{" "}
             <Link
-              href='#'
-              className='underline'
+              href="#"
+              className="underline"
               onClick={() => {
-                props.setShowLogin(false)
+                props.setShowLogin(false);
               }}
             >
               Sign up
@@ -80,7 +80,7 @@ const Login = (props: Props) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-import NavBar from '@/app/ui/navbar/navbar'
-import DestinationSelection from '@/app/ui/destination-selection/destination-selection'
+import NavBar from "@/app/ui/navbar/navbar";
+import DestinationSelection from "@/app/ui/destination-selection/destination-selection";
 
 export default function Home() {
   return (
-    <main className='flex flex-col gap-10'>
-      <div className='flex justify-between items-center'>
+    <main className="flex flex-col gap-10">
+      <div className="flex justify-between items-center">
         <NavBar />
       </div>
 
-      <div className='flex flex-col justify-center items-center h-full gap-5'>
+      <div className="flex flex-col justify-center items-center h-full gap-5">
         <DestinationSelection />
       </div>
     </main>
-  )
+  );
 }
