b103339902e59bc780d078e3e86eb74d42d9477b
Ariel Khin <u6540211@au.edu>
2024-09-01 17:13:33 +0700

    added home page for better Ui

---
+++ src/app/routes/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import NavBar from "@/app/ui/navbar/navbar";
+import DestinationSelection from "@/app/ui/destination-selection/destination-selection";
+
+
+export default function Routes() {
+
+    return (
+      <main className="flex flex-col gap-10">
+        <div className="flex justify-between items-center">
+          <NavBar />
+        </div>
+
+        <div className="flex flex-col justify-center items-center h-full gap-5">
+            <DestinationSelection />
+        </div>
+      </main>
+    );
+  } 
