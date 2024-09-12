4d5110be5355f2140fc5ed85eca2c33ac882a4b0
Jan Poonthong <janpoonthong628@gmail.com>
2024-09-12 09:11:28 +0700

    feat: navbar

--- src/app/ui/navbar/navbar.tsx
+++ src/app/ui/navbar/navbar.tsx
@@ -1,14 +1,19 @@
 'use client'
 
-import { use, useState, useEffect } from 'react'
-import logo from '@/app/navbar/logo.png'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 
 import { signOut } from 'next-auth/react'
+import { useSession } from 'next-auth/react'
+import React from 'react'
 
 export default function NavBar() {
+    const { data: session } = useSession()
+    const [userData, setUserData] = React.useState(session?.user)
+
+    console.log(userData.anem)
+
     return (
         <>
             <div>
@@ -22,20 +27,23 @@ export default function NavBar() {
                 </Link>
             </div>
 
-      <div className='flex gap-10 underline underline-offset-8 items-center'>
-        <Link href='/'>Home</Link>
-        <Link href='/mybookings'>My Bookings</Link>
-        <Link href='/profile/'>Profile</Link>
-        <Link href='/'>
-          <Button
-            onClick={() => {
-              signOut()
-            }}
-          >
-            Logout
-          </Button>
-        </Link>
-      </div>
-    </>
-  )
+            <div className="flex gap-10 underline underline-offset-8 items-center">
+                <Link href="/">Home</Link>
+                <Link href="/mybookings">My Bookings</Link>
+                <Link href="/profile/">Profile</Link>
+            </div>
+            <div className='flex gap-3 items-center'>
+                <p className="no-underline">Name: {userData.name}</p>
+                <Link href="/">
+                    <Button
+                        onClick={() => {
+                            signOut()
+                        }}
+                    >
+                        Logout
+                    </Button>
+                </Link>
+            </div>
+        </>
+    )
 }
